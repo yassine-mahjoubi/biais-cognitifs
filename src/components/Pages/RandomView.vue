e8c86f79c21124d3a05d6ff428c4856eb4a8da04
yassine mahjoubi <mahjoubi.sfx@gmail.com>
2025-12-13 15:22:10 +0100

    add router and pages

---
+++ src/components/Pages/RandomView.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+import { onMounted } from 'vue'
+import { storeToRefs } from 'pinia'
+import { useBiasStore } from '@/stores/bias'
+
+const store = useBiasStore()
+//storeToRefs keep recativity alive
+const { randomBias } = storeToRefs(store)
+const { selectRandomBias, fetchBias } = store
+onMounted(async () => {
+  await fetchBias()
+  selectRandomBias()
+})
+</script>
+<template>
+  <div>
+    <h1 v-if="randomBias" class="text-center border-l-4 text-3xl">
+      biais aléatoire: {{ randomBias.name }}
+    </h1>
+  </div>
+</template>
