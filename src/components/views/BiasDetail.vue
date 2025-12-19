f4aaaa08387b7b32041295cb7b40ce7c813e80bc
yassine mahjoubi <mahjoubi.sfx@gmail.com>
2025-12-19 18:27:39 +0100

    feat: enhance Bias components with detailed views and routing; (#30)

---
+++ src/components/views/BiasDetail.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+import { ref, onMounted } from 'vue'
+import type { Bias } from '@/type/Bias'
+import { useBiasStore } from '@/stores/bias'
+
+const store = useBiasStore()
+const { fetchBias, getBiasBySlug } = store
+const selectedBias = ref<Bias | undefined>(undefined)
+
+const propos = defineProps<{
+  slug: string
+}>()
+
+onMounted(async () => {
+  await fetchBias()
+  selectedBias.value = getBiasBySlug(propos.slug)
+})
+</script>
+
+<template>
+  <div>bias: {{ selectedBias?.description }}</div>
+</template>
