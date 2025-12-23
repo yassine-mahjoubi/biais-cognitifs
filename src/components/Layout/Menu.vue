16cde1ce2f85a0d57629f74ff2c6228aeb6faf1d
yassine mahjoubi <mahjoubi.sfx@gmail.com>
2025-12-23 01:40:48 +0100

    Develop (#35)
    * feat: enhance Bias components with detailed views and routing; (#30)
    
    * Dynamic bias views (#32)
    
    * feat: enhance Bias components with detailed views and routing;
    
    * feat: implement BiaisLayout component and refactor views to utilize slots for better structure
    
    * UI layout (#34)
    
    * feat: add categories view and enhance routing; update layout components to use slots for bias details
    
    * feat: update bias data and enhance category view; refactor routing and component structure

--- src/components/Layout/Menu.vue
+++ src/components/Layout/Menu.vue
@@ -10,6 +10,9 @@ const navLinkActiveClass = 'font-bold text-heavy-metal-50 border-b-2 border-heav
     class="sticky top-0 z-50 bg-heavy-metal-900 backdrop-blur-md border-b-4 border-heavy-metal-600 px-8 py-4 flex gap-6"
   >
     <RouterLink to="/" :class="navLinkClass" :active-class="navLinkActiveClass"> Home </RouterLink>
+    <RouterLink to="/categories" :class="navLinkClass" :active-class="navLinkActiveClass">
+      Categories de biais
+    </RouterLink>
     <RouterLink to="/liste" :class="navLinkClass" :active-class="navLinkActiveClass">
       Liste de biais
     </RouterLink>
