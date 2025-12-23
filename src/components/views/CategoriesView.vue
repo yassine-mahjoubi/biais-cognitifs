85124e2e6aae69b5081a454df61f1eb081f98722
yassine mahjoubi <mahjoubi.sfx@gmail.com>
2025-12-23 01:36:58 +0100

    UI layout (#34)
    * feat: add categories view and enhance routing; update layout components to use slots for bias details
    
    * feat: update bias data and enhance category view; refactor routing and component structure

---
+++ src/components/views/CategoriesView.vue
@@ -0,0 +1,45 @@
+<script lang="ts" setup>
+import { onMounted } from 'vue'
+import { storeToRefs } from 'pinia'
+
+import { useBiasStore } from '@/stores/bias'
+const store = useBiasStore()
+//storeToRefs keep recativity alive
+const { loading, categories } = storeToRefs(store)
+const { fetchBias } = store
+
+onMounted(async () => {
+  await fetchBias()
+})
+</script>
+
+<template>
+  <div v-if="loading">loading....</div>
+  <div v-else class="mt-4 max-w-7xl mx-auto px-4 py-8">
+    <ul class="grid grid-cols-1 md:grid-cols-3 xl:grid-cols-2 gap-8">
+      <li v-for="cat in categories" :key="cat.id">
+        <div
+          class="bg-white border-l-8 border-heavy-metal-600 p-6 rounded-lg shadow-sm hover:shadow-md transition-shadow relative h-full flex flex-col"
+        >
+          <span class="text-heavy-metal-400 text-1xl font-bold">
+            {{ cat.count }} Biais de la catégorie :</span
+          >
+          <h3 class="text-3xl font-bold text-heavy-metal-900 mt-2 mb-4 min-h-14 flex items-center">
+            {{ cat.name }}
+          </h3>
+          <p class="text-heavy-metal-700 leading-relaxed grow mb-4">
+            {{ cat.description }}
+          </p>
+
+          <RouterLink
+            :to="{ path: '/liste', query: { category: cat.id } }"
+            class="ext-heavy-metal-200 hover:text-white font-medium inline-flex items-center gap-1 self-end group relative z-10"
+          >
+            voir les {{ cat.count }} bias
+            <i aria-hidden="true" class="group-hover:translate-x-1 transition-transform">→</i>
+          </RouterLink>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
