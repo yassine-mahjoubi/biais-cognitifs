57a56754e3efcd3bdc4760e98c91432163aca6b0
yassine mahjoubi <mahjoubi.sfx@gmail.com>
2025-12-24 14:39:17 +0100

    refactor: replace Menu component with MenuNavigation and streamline BiasList functionality

--- src/components/UI/MenuFilter.vue
+++ src/components/UI/MenuFilter.vue
@@ -1,7 +1,27 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { useBiasStore } from '@/stores/bias'
+import { storeToRefs } from 'pinia'
+const store = useBiasStore()
+const { typesSort, sortedBiases } = storeToRefs(store)
+</script>
 
 <template>
   <ul>
-    <li></li>
+    <li>
+      <button
+        @click="typesSort = 'category'"
+        class="bg-heavy-metal-700 text-heavy-metal-50 rounded-2xl p-2 cursor-pointer mb-2"
+      >
+        category
+      </button>
+    </li>
+    <li>
+      <button
+        @click="typesSort = 'order'"
+        class="bg-heavy-metal-700 text-heavy-metal-50 rounded-2xl p-2 cursor-pointer mb-2"
+      >
+        order
+      </button>
+    </li>
   </ul>
 </template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -3,12 +3,10 @@ import { createRouter, createWebHistory } from 'vue-router'
 import ListeView from '@/components/views/ListeView.vue'
 import RandomView from '@/components/views/RandomView.vue'
 import BiasDetail from '@/components/views/BiasDetail.vue'
-import CategoriesView from '@/components/views/CategoriesView.vue'
 
 const routes = [
   { path: '/', component: RandomView },
   { path: '/liste', component: ListeView },
-  { path: '/categories', component: CategoriesView },
   { path: '/:slug', name: 'bias-detail', component: BiasDetail, props: true },
 ]
 const router = createRouter({
