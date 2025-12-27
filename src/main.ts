ae35c1917f18fcff621786104bb792a95f5bafd4
yassine mahjoubi <mahjoubi.sfx@gmail.com>
2025-12-27 21:14:34 +0100

    Feat add language (#43)
    * Develop (#42)
    
    * refactor: replace Menu component with MenuNavigation and streamline BiasList functionality
    
    * refactor: update MenuFilter and ListeView to use new filtering logic and types
    
    * refactor: streamline MenuFilter component and update filter handling logic
    
    * Refacto filter (#39)
    
    * refactor: replace Menu component with MenuNavigation and streamline BiasList functionality
    
    * refactor: update MenuFilter and ListeView to use new filtering logic and types
    
    * refactor: streamline MenuFilter component and update filter handling logic
    
    * refactor: simplify MenuFilter component and adjust layout in ListeView
    
    * feat: implement search functionality and reset feature in MenuFilter and SearchBias components (#41)
    
    * Add internationalization support with vue-i18n and language files

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import i18n from './plugins/i18/index'
+
 import './assets/css/main.css'
 
 import App from './App.vue'
@@ -9,5 +11,5 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(i18n)
 app.mount('#app')
