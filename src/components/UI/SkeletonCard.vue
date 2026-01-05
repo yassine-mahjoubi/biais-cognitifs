1d543fdf989e79045e786bcbe7c60bf3bd1c901c
yassine mahjoubi <mahjoubi.sfx@gmail.com>
2026-01-05 17:37:58 +0100

    feat: enhance UI components and improve loading states - Add shadcn-vue - Improved BiasList component to show skeleton loading cards while data is being fetched. - Modified ListeView to pass loading state to BiasList and added a timeout for fetching biases. - Changed initial loading state in the bias store to true and adjusted loading logic in fetchBias. - Updated tsconfig.json to correct project references and added baseUrl for path mapping. - Created components.json for shadcn-vue configuration. - Implemented SkeletonCard and Skeleton components for better loading indicators.

---
+++ src/components/UI/SkeletonCard.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+import Skeleton from '../ui/skeleton/Skeleton.vue'
+</script>
+
+<template>
+  <div class="p-6 rounded-2xl shadow-md relative h-full flex flex-col">
+    <div class="absolute top-0 right-0 w-32 h-32 opacity-10 rounded-full -mr-16 -mt-16"></div>
+    <Skeleton class="absolute h-4 w-24 rounded-md" />
+    <Skeleton class="absolute top-4 right-4 h-5 w-5 rounded-full" />
+    <div class="mt-8 mb-4 min-h-14 flex flex-col justify-center relative z-10 space-y-2">
+      <Skeleton class="h-4 w-3/4 rounded" />
+      <Skeleton class="h-4 w-1/2 rounded" />
+    </div>
+
+    <div class="leading-relaxed grow mb-4 relative z-10 space-y-2">
+      <Skeleton class="h-2 w-full rounded" />
+      <Skeleton class="h-2 w-full rounded" />
+      <Skeleton class="h-2 w-5/6 rounded" />
+    </div>
+    <Skeleton class="h-6 w-28 self-end rounded-md relative z-10" />
+  </div>
+</template>
--- src/components/views/ListeView.vue
+++ src/components/views/ListeView.vue
@@ -1,6 +1,4 @@
 <script lang="ts" setup>
-import { useI18n } from 'vue-i18n'
-const { t } = useI18n()
 import { onMounted } from 'vue'
 import { storeToRefs } from 'pinia'
 
@@ -14,15 +12,15 @@ const { loading, filteredAndSortedBiases } = storeToRefs(store)
 const { fetchBias } = store
 
 onMounted(async () => {
-  await fetchBias()
+  setTimeout(async () => {
+    await fetchBias()
+    console.log('loading: ', loading.value)
+  }, 2000)
 })
 </script>
 <template>
   <div class="max-w-7xl mx-auto px-4 py-8">
     <menu-filter />
-    <p v-if="loading">{{ t('common.loading') }}</p>
-    <template v-else>
-      <BiasList :biases="filteredAndSortedBiases" />
-    </template>
+    <BiasList :biases="filteredAndSortedBiases" :loading="loading" />
   </div>
 </template>
