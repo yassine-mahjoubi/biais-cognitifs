94833b84461ea77b8f90ee35e77e48c3e3fa9a21
yassine mahjoubi <mahjoubi.sfx@gmail.com>
2026-01-05 17:39:00 +0100

    feat: enhance UI components and improve loading states (#56)
    - Add shadcn-vue
    - Improved BiasList component to show skeleton loading cards while data is being fetched.
    - Modified ListeView to pass loading state to BiasList and added a timeout for fetching biases.
    - Changed initial loading state in the bias store to true and adjusted loading logic in fetchBias.
    - Updated tsconfig.json to correct project references and added baseUrl for path mapping.
    - Created components.json for shadcn-vue configuration.
    - Implemented SkeletonCard and Skeleton components for better loading indicators.

--- src/components/UI/BiasList.vue
+++ src/components/UI/BiasList.vue
@@ -1,15 +1,26 @@
 <script lang="ts" setup>
+import { useI18n } from 'vue-i18n'
+const { t } = useI18n()
+
 import type { Bias } from '../../type/Bias'
 import BiasItem from './BiasItem.vue'
-
+import SkeletonCard from './SkeletonCard.vue'
 defineProps<{
   biases: Bias[]
+  loading: boolean
 }>()
 </script>
 <template>
   <ul class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8">
-    <li v-for="(bias, index) in biases" :key="bias.id" :id="bias.id">
-      <BiasItem :bias="bias" :index="index" />
-    </li>
+    <template v-if="loading">
+      <div v-for="n in 12" :key="`skeleton-${n}`">
+        <SkeletonCard />
+      </div>
+    </template>
+    <template v-else>
+      <li v-for="(bias, index) in biases" :key="bias.id" :id="bias.id">
+        <BiasItem :bias="bias" :index="index" />
+      </li>
+    </template>
   </ul>
 </template>
