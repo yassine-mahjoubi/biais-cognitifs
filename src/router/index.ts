08ff407583ae45b8e1ca260f05d47fa7a26cfcca
yassine mahjoubi <mahjoubi.sfx@gmail.com>
2026-01-08 23:24:50 +0100

    Add About page and update json data source (#60)
    * feat: add About page and update navigation links
    
    * update json data french and english and add new bias action

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,10 +3,12 @@ import { createRouter, createWebHistory } from 'vue-router'
 import ListeView from '@/components/views/ListeView.vue'
 import RandomView from '@/components/views/RandomView.vue'
 import BiasDetail from '@/components/views/BiasDetail.vue'
+import AboutView from '@/components/views/AboutView.vue'
 
 const routes = [
   { path: '/', component: RandomView },
   { path: '/liste', component: ListeView },
+  { path: '/about', component: AboutView },
   { path: '/:slug', name: 'bias-detail', component: BiasDetail, props: true },
 ]
 const router = createRouter({
