fd35f51e6575bbc4cf6501f9db6df7e21861edd9
yassine mahjoubi <mahjoubi.sfx@gmail.com>
2026-01-09 11:24:49 +0100

    feat: add footer component and update translations for footer text (#62)

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
+import FooterInfos from './components/Layout/FooterInfos.vue'
 import MenuNavigation from './components/Layout/MenuNavigation.vue'
 </script>
 <template>
-  <div class="min-h-screen bg-heavy-metal-50">
+  <div class="min-h-screen bg-heavy-metal-50 flex flex-col">
     <menu-navigation />
-    <main>
+    <main class="flex-1">
       <RouterView />
     </main>
+    <FooterInfos />
   </div>
 </template>
---
+++ src/components/Layout/FooterInfos.vue
@@ -0,0 +1,56 @@
+<script lang="ts" setup>
+import { Icon } from '@iconify/vue'
+import { useI18n } from 'vue-i18n'
+const { t } = useI18n()
+</script>
+
+<template>
+  <footer
+    role="contentInfo"
+    class="bg-heavy-metal-950 text-heavy-metal-200 flex justify-between items-center px-8 py-4"
+  >
+    <div class="inline-flex items-center gap-2">
+      <span>{{ t('footer.made') }}</span>
+      <Icon
+        icon="arcticons:brain-out"
+        width="24"
+        height="24"
+        style="color: #ff3939"
+        aria-hidden="true"
+      />
+    </div>
+    <div>
+      <ul class="flex justify-center gap-3 items-center">
+        <li>
+          <a
+            href="https://github.com/yassine-mahjoubi/biais-cognitifs"
+            target="_blank"
+            :title="'GIthub ' + t('footer.external_link')"
+          >
+            <Icon
+              aria-hidden="true"
+              class="icon"
+              icon="streamline-logos:github-logo-2-solid"
+              width="24"
+              height="24"
+            />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/yassine-mahjoubi/"
+            target="_blank"
+            :title="'Linkedin ' + t('footer.external_link')"
+          >
+            <Icon
+              icon="entypo-social:linkedin-with-circle"
+              width="24"
+              height="24"
+              style="color: #fff"
+            />
+          </a>
+        </li>
+      </ul>
+    </div>
+  </footer>
+</template>
