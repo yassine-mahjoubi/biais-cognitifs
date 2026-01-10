443f4ecc0ff7b64613d38aa2dd3079c4be40b1d0
yassine mahjoubi <mahjoubi.sfx@gmail.com>
2026-01-10 17:28:20 +0100

    feat: update pagination logic and improve accessibility in BiasList and SearchBias components

--- src/components/UI/BiasList.vue
+++ src/components/UI/BiasList.vue
@@ -11,9 +11,10 @@ const propos = defineProps<{
   biases: Bias[]
   loading: boolean
 }>()
-const totalPages = computed(() => Math.ceil(propos.biases.length / 10))
 const currentPage = ref<number>(1)
-const paginateBy = 10
+const paginateBy = 20
+const totalPages = computed(() => Math.ceil(propos.biases.length / paginateBy))
+
 const nextPage = () => {
   currentPage.value++
 }
@@ -25,47 +26,12 @@ const displayedPages = computed(() => {
   const end = start + paginateBy
   return propos.biases.slice(start, end)
 })
+const goToPage = (page: number) => (currentPage.value = page)
 </script>
 <template>
-  total pages: {{ totalPages }}
-  <nav aria-label="paginaation" aria-labelledby="pagination">
-    <h2 class="sr-only" id="pagination">pagination</h2>
-    <ul class="mb-5 flex justify-center">
-      <li>
-        <a
-          v-if="currentPage > 1"
-          @click="previousPage()"
-          class="bg-heavy-metal-950 text-heavy-metal-200 rounded-2xl cursor-pointer px-4 py-2 inline-flex group"
-        >
-          <i aria-hidden="true" class="group-hover:-translate-x-1 transition-transform">←</i>
-          <span> {{ t('buttons.previous') }} </span>
-        </a>
-      </li>
-      <li v-for="page in totalPages" :key="`id-${page}`">
-        <a
-          href=""
-          :aria-current="currentPage === page ? 'page' : undefined"
-          class="bg-amber-700 text-shadow-heavy-metal-300 border cursor-pointer"
-          :class="currentPage === page ? 'bg-black text-white' : ''"
-          >{{ page }}
-        </a>
-      </li>
-
-      <li>
-        <a
-          v-if="currentPage < totalPages"
-          @click="nextPage()"
-          class="bg-heavy-metal-950 text-heavy-metal-200 rounded-2xl cursor-pointer px-4 py-2 inline-flex items-center gap-1 group"
-        >
-          <span> {{ t('buttons.next') }} </span>
-          <i aria-hidden="true" class="group-hover:translate-x-1 transition-transform">→</i>
-        </a>
-      </li>
-    </ul>
-  </nav>
   <ul class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8">
     <template v-if="loading">
-      <div v-for="n in 10" :key="`skeleton-${n}`">
+      <div v-for="n in paginateBy" :key="`skeleton-${n}`">
         <SkeletonCard />
       </div>
     </template>
@@ -75,4 +41,39 @@ const displayedPages = computed(() => {
       </li>
     </template>
   </ul>
+  <nav aria-label="pagiaation" role="navigation">
+    <ul class="mb-5 flex justify-center mt-5">
+      <li>
+        <a href="#" v-if="currentPage > 1" @click="previousPage()" class="">
+          <i aria-hidden="true" class="group-hover:-translate-x-1 transition-transform">←</i>
+          <span> {{ t('buttons.previous') }} </span>
+        </a>
+      </li>
+      <li v-for="page in totalPages" :key="`id-${page}`">
+        <a
+          :href="`?page=${page}`"
+          @click.prevent="goToPage(page)"
+          :aria-current="currentPage === page ? 'page' : undefined"
+          :title="`page ${page}`"
+          class=""
+          :class="currentPage === page ? 'bg-heavy-metal-800 text-heavy-metal-100' : ''"
+          >{{ page }}
+        </a>
+      </li>
+
+      <li>
+        <a href="#" v-if="currentPage < totalPages" @click="nextPage()" class="">
+          <span> {{ t('buttons.next') }} </span>
+          <i aria-hidden="true" class="group-hover:translate-x-1 transition-transform">→</i>
+        </a>
+      </li>
+    </ul>
+  </nav>
 </template>
+<style lang="css" scoped>
+a {
+  padding: 5px;
+  border: 1px solid var(--bg-heavy-metal-800);
+  cursor: pointer;
+}
+</style>
