5b6c887096d979e7e69a9619f8a36d65ecbafd1a
yassine mahjoubi <mahjoubi.sfx@gmail.com>
2026-01-31 18:11:41 +0100

    Feat/add pwa (#72)
    * feat: add PWA support with offline mode and install capability
    
    * feat: update README to reflect PWA features and source link changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 const defineBasePath = () => {
@@ -16,7 +17,56 @@ const defineBasePath = () => {
 // https://vite.dev/config/
 export default defineConfig({
   base: defineBasePath(),
-  plugins: [vue(), vueDevTools(), tailwindcss()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', '**/*.json'],
+      manifest: {
+        name: 'Biais Cognitifs | My Biais',
+        short_name: 'My Biais',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'icons/android-launchericon-48-48.png',
+            sizes: '48x48',
+            type: 'image/png',
+          },
+          {
+            src: 'icons/android-launchericon-72-72.png',
+            sizes: '72x72',
+            type: 'image/png',
+          },
+          {
+            src: 'icons/android-launchericon-96-96.png',
+            sizes: '96x96',
+            type: 'image/png',
+          },
+          {
+            src: 'icons/android-launchericon-144-144.png',
+            sizes: '144x144',
+            type: 'image/png',
+          },
+          {
+            src: 'icons/android-launchericon-192-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'icons/android-launchericon-512-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+      workbox: {
+        // all cached  data, even JSON at /public
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,json}'],
+      },
+    }),
+    vueDevTools(),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
